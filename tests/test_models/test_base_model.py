030b5a0540a6dacb4896835726ac147ba01e2d34
sumutoni <steumutoni@gmail.com>
2022-11-22 09:35:01 -0800

    modified __init__ to use args and kwargs

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,7 @@
 import unittest
 import sys
 sys.path.append('../..')
+from io import StringIO
 from models.base_model import BaseModel
 from unittest.mock import patch, call
 """unit test for BaseModel class"""
@@ -29,6 +30,11 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(self.model1.created_at, self.model1.updated_at)
         self.model2.save()
         self.assertNotEqual(self.model2.created_at, self.model2.updated_at)
+
+    def testinit(self):
+        model_dict = self.model1.to_dict()
+        model3 = BaseModel(**model_dict)
+        self.assertEqual(model3.to_dict(), model_dict)
 
 if __name__ == '__main__':
     unittest.main()
