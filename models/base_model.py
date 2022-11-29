75dcd56096d5ad4ca8931f0770642c2d68c7d6b9
sumutoni <steumutoni@gmail.com>
2022-11-29 05:17:30 -0800

    modified the str method

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
 
     def __str__(self):
         """prints representation of instance"""
-        print("[{}] ({}) {}".format(self.__class__.__name__,
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
                                     self.id, self.__dict__))
 
     def save(self):
