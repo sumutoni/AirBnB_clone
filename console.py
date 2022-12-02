826b3ef0f96255a36fcc376734ff793a64c39a20
ZIHCO <chizobajames21@gmail.com>
2022-12-02 21:12:32 +0000

    fixes

--- console.py
+++ console.py
@@ -127,8 +127,4 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        HBNBCommand().onecmd(' '.join(sys.argv[1:]))
-    else:
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
